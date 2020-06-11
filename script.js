dd8c029aa00fe7245da89370f874e9a9e873a4ed
Sarah Kerr <sarah.kerr93@gmail.com>
2020-06-11 18:32:12 -0400

    added forms to HTML

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("password");
+var statisticsElem = document.getElementById("statistics");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  
+  passwordText.value = retVal;
 
+}
+
+function generatePassword() {
+  var length = document.getElementById("selectNum").input
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
 }
 
 // Add event listener to generate button
