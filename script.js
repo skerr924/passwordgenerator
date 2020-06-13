777a836200727a0d93dfc67d8845de4109a40e0e
Sarah Kerr <sarah.kerr93@gmail.com>
2020-06-13 16:58:20 -0400

    clarifies the functionality of the js files with additional comments

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var includeSpecial = document.querySelector("#includeSpecial");
 var charset = "";
 var retVal;
 
-// Write password to the #password input
+// Write password to the #password output box 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -17,13 +17,12 @@ function writePassword() {
 }
 
 //Determine character set based on user inputs 
-
-
 function generateCharset() {
-  //some qualifying information is required 
+  //ensure that qualifying information is provided by checking some of the requirement boxes are filled out 
   if (includeLowercase.checked === true || includeUppercase.checked === true ||
     includeNumbers.checked === true || includeSpecial.checked === true) {
 
+      //adds elements to the charset depending on which checkmarks are selected 
     if (includeLowercase.checked) {
       charset += "abcdefghijklmnopqrstuvwxyz";
     }
@@ -40,7 +39,7 @@ function generateCharset() {
 
   }
 
-  //alerts user that sufficient information was not provided 
+  //alerts user if sufficient information was not provided 
   else {
     alert("Looks like you didn't give us enough information. What type of characters do you need in your password?");
   }
@@ -51,15 +50,17 @@ function generatePassword() {
   var retVal = "";
   var length = document.querySelector(".selectNum").value;
   if (length < 8 || length > 128) {
-    // return("Please select a password length between 8 and 128");
+    // generates alert to prompt the user to go back and select an appropriate character length
     alert("Oops! We only generate password between 8 and 128 characters long. Try again!");
   }
+  //generates the character set of the correct/requested length 
   else {
     generateCharset();
     for (var i = 0; i < length; i++) {
       retVal += charset.charAt(Math.floor(Math.random() * charset.length));
     }
   }
+  //returns the value to the output box 
   return retVal;
 }
 
